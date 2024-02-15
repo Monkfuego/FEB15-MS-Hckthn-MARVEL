1470f2c56f3c8a15c4d4f64e1b110b6dab2272fc
Joseph Remingston <ljremi@gmail.com>
2024-02-15 22:10:24 +0530

    updated

--- server.js
+++ server.js
@@ -11,8 +11,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Session middleware with a random secret key and MongoDB store
-
-
+var fs = require("fs")
+var image1 = fs.readFileSync("RDT_20240215_181550740786580592949787.jpg")
+var image2 = fs.readFileSync("RDT_20240215_1813402010455412332883482.jpg")
+var image3 = fs.readFileSync("RDT_20240215_181550740786580592949787.jpg")
+var image4 = fs.readFileSync("RDT_20240215_1813402010455412332883482.jpg")
+var base64Image1 = image1.toString('base64')
+var base64Image2 = image2.toString('base64')
+var base64Image3 = image3.toString('base64')
+var base64Image4 = image4.toString('base64')
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 var userSchema = new mongoose.Schema({
@@ -21,7 +28,14 @@ var userSchema = new mongoose.Schema({
   email: String,
 });
 
+var imageSchema = new mongoose.Schema({
+  id : Number,
+  image : String,
+  dec : String
+});
+
 var users = mongoose.model("users", userSchema);
+var images = mongoose.model("buying" , imageSchema)
 app.get("/", (req, res) => {
   res.set({
     "Allow-access-Allow-Origin": "*",
@@ -61,6 +75,7 @@ app.post("/signin", async (req, res) => {
   var { username, password } = req.body;
   try {
     var user = await users.findOne({ username: username });
+    module.exports = user
 
     if (user) {
       var pass = await bcrypt.compare(password, user.password);
@@ -84,3 +99,24 @@ var PORT = process.env.PORT || 200;
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
 });
+
+var imgs1 = {
+  id : 1,
+  image : base64Image1
+}
+var imgs2 = {
+  id : 2,
+  image : base64Image2
+}
+var imgs3 = {
+  id : 3,
+  image : base64Image3
+}
+var imgs4 = {
+  id : 4,
+  image : base64Image4
+}
+users.create(imgs1)
+users.create(imgs2)
+users.create(imgs3)
+users.create(imgs4)
