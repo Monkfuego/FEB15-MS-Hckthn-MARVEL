7b5a52d47ca9a9d7726caff208dd294c1c734085
Joseph Remingston <ljremi@gmail.com>
2024-02-15 18:03:37 +0530

    updated

--- server.js
+++ server.js
@@ -1,73 +1,85 @@
-var bodyparser = require("body-parser")
-var express = require("express")
-var app = express()
-var bcrypt = require("bcrypt")
-var mongoose = require("mongoose")
-app.use(express.static(__dirname + "/"))
-app.use(bodyparser.json())
-require("dotenv").config()
-var PORT = process.env.PORT || 200
-var users = require("./models/users")
-app.use(bodyparser.urlencoded({extended : true}))
-mongoose.connect(process.env.MONGO_URI)
-app.get("/" , (req , res) => {
-    res.set({
-        "Allow-access-Allow-Origin" : "*"
-    })
-    return res.redirect("./signin.html")
-})
+// Import necessary modules
+var bodyParser = require("body-parser");
+var express = require("express");
+var app = express();
+var bcrypt = require("bcrypt");
+var mongoose = require("mongoose");
+require("dotenv").config();
+
+// Use middleware
+app.use(express.static(__dirname + "/"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define the user schema and model
+var userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  email: String,
+});
+
+var users = mongoose.model("users", userSchema);
+
+// Define routes
+app.get("/", (req, res) => {
+  res.set({
+    "Allow-access-Allow-Origin": "*",
+  });
+  return res.redirect("/signin.html");
+});
 
 app.post("/signup", async (req, res) => {
-    var usernamemain = req.body.username;
-    var password = req.body.password;
-    var confirmPassword = req.body.confirmPassword;
-    var mail = req.body.email
+  var { username, password, confirmPassword, email } = req.body;
 
-  
-    if (toString(password) === toString(confirmPassword)) {
-      const hashedPassword = await bcrypt.hash(password, 10);
-  
-      var data = {
-        username: usernamemain,
-        password: hashedPassword,
-        email : mail  
-        
-      };
-      try{
-        await users.insertMany(data)
-        res.redirect("./signin.html")
-      }
-      catch (err){
-        throw err
+  if (password === confirmPassword) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    var data = {
+      username: username,
+      password: hashedPassword,
+      email: email,
+    };
+
+    try {
+      await users.create(data);
+      res.redirect("./signin.html");
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    }
+  } else {
+    res.status(400).send("Password not matching");
+  }
+});
+
+app.post("/signin", async (req, res) => {
+  var { username, password } = req.body;
+
+  try {
+    var user = await users.findOne({ username: username });
+
+    if (user) {
+      var pass = await bcrypt.compare(password, user.password);
+
+      if (pass) {
+        res.send("logged in");
+      } else {
+        res.redirect("signin.html");
       }
     } else {
-      res.status(400).send("Password not matching");
+      res.redirect("signin.html");
     }
-  });
-
-app.post("/signin" , async (req , res) => {
-  var usernamemain = req.body.username
-  var passwords = req.body.password
-  var dbstored = users.find({})
-  for (var i = 0 ; i < dbstored.length ; i++){
-    var ele = dbstored[i]
-    try{
-      if (dbstored){
-        var pass = await bcrypt.compare(passwords , ele.password)
-        console.log(pass)
-        if(pass == true){
-          res.redirect("signed in")
-        }
-      }
-      res.redirect("signin.html")
-    }
-    catch(err){
-      console.log(err)
-    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
   }
-})
-  
+});
 
-app.listen(PORT , () => {
-    console.log("server")
-})
+// Start the server
+var PORT = process.env.PORT || 200;
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
