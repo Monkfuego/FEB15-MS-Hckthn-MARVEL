4c979336ed7e4bee0f560d24d891644037dd407b
Joseph Remingston <ljremi@gmail.com>
2024-02-15 21:20:49 +0530

    updated

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 var bodyParser = require("body-parser");
 var express = require("express");
-var app = express();
+var session = require("express-session");
 var bcrypt = require("bcrypt");
 var mongoose = require("mongoose");
 require("dotenv").config();
+
+var app = express();
 app.use(express.static(__dirname + "/"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Session middleware with a random secret key and MongoDB store
+
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 var userSchema = new mongoose.Schema({
   username: String,
   password: String,
   email: String,
 });
+
 var users = mongoose.model("users", userSchema);
 app.get("/", (req, res) => {
   res.set({
@@ -20,11 +28,13 @@ app.get("/", (req, res) => {
   });
   return res.redirect("/signin.html");
 });
+
+// Signup route
 app.post("/signup", async (req, res) => {
-  var username = req.body.username
-  var password = req.body.password
-  var confirmPassword = req.body.confirmPassword
-  var email = req.body.email
+  var username = req.body.username;
+  var password = req.body.password;
+  var confirmPassword = req.body.confirmPassword;
+  var email = req.body.email;
 
   if (password === confirmPassword) {
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -45,6 +55,8 @@ app.post("/signup", async (req, res) => {
     res.status(400).send("Password not matching");
   }
 });
+
+// Signin route
 app.post("/signin", async (req, res) => {
   var { username, password } = req.body;
   try {
@@ -52,20 +64,22 @@ app.post("/signin", async (req, res) => {
 
     if (user) {
       var pass = await bcrypt.compare(password, user.password);
-
       if (pass) {
-        res.redirect("./strange.html")
-      } else {
-        res.redirect("signin.html");
+        return res.redirect("./strange.html");
       }
-    } else {
-      res.redirect("signin.html");
+      else if(!pass){
+        return res.status(401).send("Incorrect password");
+      }
+    else {
+      return res.status(404).send("User not found");
     }
+  }
   } catch (err) {
     console.error(err);
-    res.status(500).send("Internal Server Error");
+    return res.status(500).send("Internal Server Error");
   }
 });
+
 var PORT = process.env.PORT || 200;
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
