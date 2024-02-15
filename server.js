6e8eef8237a20de2723a269c5546b3331d9e0291
Joseph Remingston <ljremi@gmail.com>
2024-02-15 19:10:22 +0530

    updated

--- server.js
+++ server.js
@@ -1,38 +1,30 @@
-// Import necessary modules
 var bodyParser = require("body-parser");
 var express = require("express");
 var app = express();
 var bcrypt = require("bcrypt");
 var mongoose = require("mongoose");
 require("dotenv").config();
-
-// Use middleware
 app.use(express.static(__dirname + "/"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Define the user schema and model
 var userSchema = new mongoose.Schema({
   username: String,
   password: String,
   email: String,
 });
-
 var users = mongoose.model("users", userSchema);
-
-// Define routes
 app.get("/", (req, res) => {
   res.set({
     "Allow-access-Allow-Origin": "*",
   });
   return res.redirect("/signin.html");
 });
-
 app.post("/signup", async (req, res) => {
-  var { username, password, confirmPassword, email } = req.body;
+  var username = req.body.username
+  var password = req.body.password
+  var confirmPassword = req.body.confirmPassword
+  var email = req.body.email
 
   if (password === confirmPassword) {
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -42,7 +34,6 @@ app.post("/signup", async (req, res) => {
       password: hashedPassword,
       email: email,
     };
-
     try {
       await users.create(data);
       res.redirect("./signin.html");
@@ -54,10 +45,8 @@ app.post("/signup", async (req, res) => {
     res.status(400).send("Password not matching");
   }
 });
-
 app.post("/signin", async (req, res) => {
   var { username, password } = req.body;
-
   try {
     var user = await users.findOne({ username: username });
 
@@ -77,8 +66,6 @@ app.post("/signin", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
-
-// Start the server
 var PORT = process.env.PORT || 200;
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
